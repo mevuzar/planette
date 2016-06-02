f83d73d9423b45395dae3caa1a0c0157c7da6f98
mevuzar <distributed.programmer@gmail.com>
2016-06-02 17:25:46 +0300

    Kleisli

--- src/main/scala-2.11/fddd.book/chapter_four/kleisli/Trading.scala
+++ src/main/scala-2.11/fddd.book/chapter_four/kleisli/Trading.scala
@@ -1,6 +1,8 @@
-package fddd.book.chapter_four.trading
+package fddd.book.chapter_four.kleisli
 
-import scalaz.Kleisli
+
+import scalaz._
+import Scalaz._
 
 /**
  * @author yoav @since 6/2/16.
@@ -21,12 +23,19 @@ object TradingServicePlain extends TradingServicePlain[ClientOrder, Order, Marke
   override def allocate(accounts: List[Account]): (Execution) => List[Trade] = ???
 }
 
-object TradingServiceKleisli extends TradingServiceKleisli[ClientOrder, Order, Market, Execution, Trade, Account]{
-  override def clientOrders: Kleisli[List, ClientOrder, Order] = ???
+object TradingServiceKleisli extends TradingServiceKleisli[Map[String, String], Order, Market, Execution, Trade, String]{
+  override def clientOrders: Kleisli[List, Map[String, String], Order] = ???
 
-  override def execute(market: Market, broker: Account): Kleisli[List, Order, Execution] = ???
+  override def execute(market: Market, broker: String): Kleisli[List, Order, Execution] = ???
 
-  override def allocate(accounts: List[Account]): Kleisli[List, Execution, Trade] = ???
+  override def allocate(accounts: List[String]): Kleisli[List, Execution, Trade] = ???
+
+  case object HongKong extends Market
+
+  val tradeGenerator = tradeGeneration(HongKong, "jojo", List("jbabo", "nisso", "miko"))
+  val result = tradeGenerator run (Map("instrument" -> "juju/2/4-jaja/5/6", "no" -> "12345", "customer" -> "your mom!"))
+  result
+
 }
 
 
@@ -40,4 +49,11 @@ trait TradingServiceKleisli[ClientOrder, Order, Market, Execution, Trade, Accoun
   def clientOrders: Kleisli[List,ClientOrder,Order]
   def execute(market: Market, broker: Account): Kleisli[List,Order,Execution]
   def allocate(accounts: List[Account]): Kleisli[List,Execution,Trade]
+
+  def tradeGeneration(market: Market, broker: Account, clientAccounts: List[Account]) = {
+    clientOrders               andThen
+      execute(market, broker)    andThen
+      allocate(clientAccounts)
+  }
 }
+
---
+++ src/main/scala-2.11/fddd.book/chapter_four/kleisli/WorkWithKleisli.scala
@@ -0,0 +1,60 @@
+package fddd.book.chapter_four.kleisli
+
+/**
+ * @author yoav @since 6/2/16.
+ */
+import scalaz.Kleisli._
+import scalaz.Scalaz._
+import scalaz.{Order => OrderZ, _}
+
+
+object WorkWithKleisli extends App {
+  def listIntToListString: List[Int] => List[String] = ints => ints.map(_.toString)
+
+  def listStringToOptionStringThroughComputation(computation: String => Boolean): String => List[Option[String]] =
+    str => List(Some(str))
+
+  def optionStringToComputedInt(computation: Int => Int): Option[String] => List[Int] = op =>{
+    op.map(i => Range(0, i.toInt).map(computation)).toList.flatten
+  }
+
+  val listIntToListStringKli = kleisli(listIntToListString)
+  val stringToOptionFilteredKli = kleisli(listStringToOptionStringThroughComputation(s => s.toInt < 4))
+  val optionStringToComputedIntKli = kleisli(optionStringToComputedInt(i => i*10))
+
+  val composed = listIntToListStringKli andThen stringToOptionFilteredKli andThen optionStringToComputedIntKli
+  val result = composed run(List(1, 2, 3, 4, 5))
+  println(s"composed kleisli: $result")
+
+}
+
+//trait TradingInterpreter extends Trading[Account, Trade, ClientOrder, Order, Execution, Market] {
+//
+//  def clientOrders: Kleisli[List, List[ClientOrder], Order] = kleisli(fromClientOrders)
+//
+//  def execute(market: Market, brokerAccount: Account) = kleisli[List, Order, Execution] { order =>
+//    order.items.map { item =>
+//      Execution(brokerAccount, item.ins, "e-123", market, item.price, item.qty)
+//    }
+//  }
+//
+//  def allocate(accounts: List[Account]) = kleisli[List, Execution, Trade] { execution =>
+//    val q = execution.quantity / accounts.size
+//    accounts.map { account =>
+//      makeTrade(account, execution.instrument, "t-123", execution.market, execution.unitPrice, q)
+//    }
+//  }
+//}
+//
+//object TradingInterpreter extends TradingInterpreter
+//
+//object Example extends App {
+//  val market = HongKong
+//
+//  val tradeGenerator = TradingInterpreter.tradeGeneration(HongKong, "jojo", List("jbabo", "nisso", "miko"))
+//  val result = tradeGenerator run (List(Map("instrument" -> "juju/2/4-jaja/5/6", "no" -> "12345", "customer" -> "your mom!")))
+//
+//  println(result)
+//
+//
+//}
