4f5eb3468bb5365f2f730fba4aab6ab7f17fa55f
mevuzar <distributed.programmer@gmail.com>
2016-07-01 05:41:29 +0300

    Straightening UserClient out

--- src/main/scala-2.11/com/mayo/planette/product/client/domain/UserClient.scala
+++ src/main/scala-2.11/com/mayo/planette/product/client/domain/UserClient.scala
@@ -4,13 +4,14 @@ package domain
 /**
  * @author yoav @since 6/16/16.
  */
-trait UserClient extends 
-ClientAccountsService with
-ClientPlanningService with
-Discovery with 
-Lessons with 
-ClientWishlistsService with
-ClientRecipesService{
+trait UserClient {
+  val accountService: ClientAccountsService
+  val planningService: ClientPlanningService
+  val discoveryService: Discovery
+  val lessonsService: Lessons
+  val wishlistsService: ClientWishlistsService
+  val recipesService: ClientRecipesService
+
   def appStart: Unit
 
   def show[A](a: A): Unit
